edfbc931d494720ca3fc5af1b0f9d1a5f0d92a3f
Ghost <culestfrosh@gmail.com>
2023-05-23 08:23:13 +0100

    register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,14 @@
+import {
+  Typography
+} from "@mui/material"
+
+const Register = () => {
+   return(
+    <div>
+     <Typography variant="h1">Register page</Typography>
+    </div>
+    )
+}
+
+
+export default Register;
