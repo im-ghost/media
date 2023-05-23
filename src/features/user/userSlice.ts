ca2b10ecab0bdd1d9f205defb2456617f4ff2254
Ghost <culestfrosh@gmail.com>
2023-05-23 08:22:06 +0100

    fixed type bug*

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState} from '../../app/store';
+
+import type { USER } from "../../app/types"
+type UserState = {
+  userInfo: null | USER
+  status: 'idle' | 'loading' | 'failed';
+}
+const userInfoL:string | null = localStorage.getItem("media-user")
+const initialState: UserState = {
+  userInfo: userInfoL ? JSON.parse(userInfoL) : null,
+  status: 'idle',
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    setUser: (state, action: PayloadAction<USER>) => {
+    localStorage.setItem("media-user",JSON.stringify(action.payload))
+      state.userInfo = action.payload;
+    }
+  },
+  
+});
+
+export const { setUser } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user.userInfo;
+
+
+export default userSlice.reducer;
