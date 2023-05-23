aa0210e4ceb84b8f3e3e66c40e61ab0f33a2c411
Ghost <culestfrosh@gmail.com>
2023-05-23 08:19:35 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React,{
   useEffect
 } from 'react';
 import eruda from "eruda";
-import { Counter } from './features/counter/Counter';
 function App() {
   useEffect(()=>{
     eruda.init()
   },[])
   return (
     <div className="App">
-        <Counter />
+        <h1>hhhhh</h1>
     </div>
   );
 }
