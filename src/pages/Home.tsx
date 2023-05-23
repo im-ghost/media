c10e90964a73ca2bc18e84bb49f55a10eb417411
Ghost <culestfrosh@gmail.com>
2023-05-23 08:23:27 +0100

    home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import {
+  Typography
+} from "@mui/material"
+
+const Home = () => {
+  return(
+    <div>
+     <Typography variant="h1">Home page</Typography>
+    </div>
+    )
+}
+
+
+export default Home;
