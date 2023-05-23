891e6d130e659234e0e6903b3ebb5f7a2ba057f9
Ghost <culestfrosh@gmail.com>
2023-05-23 08:23:56 +0100

    logout page

---
+++ src/pages/Logout.tsx
@@ -0,0 +1,14 @@
+import {
+  Typography
+} from "@mui/material"
+
+const Logout = () => {
+   return(
+    <div>
+     <Typography variant="h1">Logout page</Typography>
+    </div>
+    )
+}
+
+
+export default Logout;
