9394d7233e7433678927d5a325b4016e3588c4cb
Ghost <culestfrosh@gmail.com>
2023-05-23 13:06:41 +0100

    added toastify

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React,{
   useEffect
 } from 'react';
 import eruda from "eruda";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 function App() {
   useEffect(()=>{
     eruda.init()
   },[])
   return (
     <div className="App">
+    <ToastContainer />
         <h1>hhhhh</h1>
+        <Outlet />
     </div>
   );
 }
