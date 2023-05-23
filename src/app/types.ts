3bbf06938ea20a3a76255db472dd62e8743f3b11
Ghost <culestfrosh@gmail.com>
2023-05-23 08:21:09 +0100

    added type to the export

---
+++ src/app/types.ts
@@ -0,0 +1,89 @@
+// types file
+
+// User type
+type USER = {
+  name: String,
+  phone:  String,
+  email: String
+  chats: [String],
+  bio: String,
+  followers: [String],
+  following: [String],
+  posts: [String],
+  password: String
+}
+
+// Users ,an array of user type
+type USERS = [USER]
+
+//CHAT type
+type CHAT = {
+  name: String,
+  members: [String],
+  messages: [String],
+  bio?:String,
+  image?:String
+}
+
+//CHATS type
+type CHATS = [CHAT]
+
+//Post type
+type POST = {
+  author: [String],
+  comments: [String],
+  image: String,
+  caption: String,
+  likes:[String],
+  content:  String,
+  retweets: [String]
+}
+
+//POSTS type
+type POSTS = POST[]
+
+//Comment type
+type COMMENT = { 
+   author: String,
+  post: String,
+  content: String,
+  likes:[String]
+}
+
+//Comments type
+type COMMENTS = [COMMENT]
+
+//Like type
+type LIKE = {
+  author: String,
+  post:String
+}
+
+//Likes type
+type LIKES = [LIKE]
+
+//Message type
+type MESSAGE = {
+  sender: String,
+  chat: String,
+  content:String,
+  timestamp: Date,
+}
+
+//messages type
+type MESSAGES = MESSAGE[]
+
+export type {
+  MESSAGE,
+  MESSAGES,
+  LIKES,
+  LIKE,
+  COMMENTS,
+  COMMENT,
+  POST,
+  POSTS,
+  CHAT,
+  CHATS,
+  USERS,
+  USER
+}
