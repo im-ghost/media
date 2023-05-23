a3b5001f66a3df8a2a9717d8f0eece4d7c71de43
Ghost <culestfrosh@gmail.com>
2023-05-23 08:23:42 +0100

    login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import {
+  Typography
+} from "@mui/material"
+
+const Login = () => {
+   return(
+    <div>
+     <Typography variant="h1">Login page</Typography>
+    </div>
+    )
+}
+
+
+export default Login;
