372409621dc709036c23fdf324bcd2279617dca7
Ghost <culestfrosh@gmail.com>
2023-05-23 08:21:39 +0100

    index file, with new routing*

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,37 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import Home from "./pages/Home"
+import Logout from "./pages/Logout"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Profile from "./pages/Profile"
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements
+} from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+     <Route index path="/" element={<Home/>}/>
+     <Route path="/login" element={<Login/>}/>
+     <Route path="/logout" element={<Logout/>}/>
+     <Route path="/register" element={<Register/>}/>
+     <Route path="/profile" element={<Profile/>}/>
+    </Route>
+    )
+  )
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
