0122a93a85c050e917679923ae0d30124f9fc2be
Ghost <culestfrosh@gmail.com>
2023-05-26 20:28:32 +0100

    added service worker registrstion

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@ import {
   createRoutesFromElements
 } from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 
 const container = document.getElementById('root')!;
@@ -37,6 +38,10 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
