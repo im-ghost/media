1f0bc6beb365c64b30a8d980bac83f9d4104d4ba
Ghost <culestfrosh@gmail.com>
2023-05-26 21:19:35 +0100

    fixed Bug

--- src/components/HomePageNavbar.tsx
+++ src/components/HomePageNavbar.tsx
@@ -90,6 +90,56 @@ const Navbar:React.FC<{user:USER | null}> = ({user}):JSX.Element => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
+const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+     <MenuItem  onClick={()=> navigate("/chats")}>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            💬
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem  onClick={()=> navigate("/notifications")}>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            🔕
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem  onClick={()=> navigate("/profile")}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          🙍
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   const mobileMenuId = 'primary-search-account-menu-mobile';
   const renderMobileMenu = (
