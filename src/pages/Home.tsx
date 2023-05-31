ce4a78d1b0f01caeac71705e357032d289c848d8
Ghost <culestfrosh@gmail.com>
2023-05-31 08:29:33 +0100

    fixed the bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,42 @@
-
-import { Outlet} from "react-router-dom"
-import { useAppSelector,useAppDispatch } from "../app/store";
-import {
-  useNavigate,
-  useParams
-} from "react-router-dom";
-import type {
-  USER
-} from "../app/types"
+import React, { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { useAppSelector } from "../app/store";
+import type { USER } from "../app/types";
 import Navbar from "../components/HomePageNavbar";
-const Home = () => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const userFromStore = useAppSelector(state=>state.user.userInfo)
-return(
-  <div>
-  <Navbar user={userFromStore} />
-     <Outlet />
-    </div>
-    )
-}
+import { toast } from "react-toastify";
+import Posts from "../components/posts";
+import { useAllUsersQuery } from "../features/user/userApiSlice";
 
+const Home = () => {
+  const userFromStore = useAppSelector((state) => state.user.userInfo);
+  const [posts, setPosts] = useState<string[]>([]);
+// ts-ignore
+  const { data, error } = useAllUsersQuery(); // Call the hook directly
+
+  useEffect(() => {
+    if (data) {
+      const fetchedPosts: string[] = data.reduce((acc: string[], user: USER) => {
+        if (user.posts) {
+          acc.push(...user.posts);
+        }
+        return acc;
+      }, []);
+
+      if (fetchedPosts.length > 0) {
+        setPosts(fetchedPosts);
+      }
+    } else {
+      toast.error(JSON.stringify(error));
+    }
+  }, [data, error]);
+
+  return (
+    <div>
+      <Navbar user={userFromStore} />
+      {posts.length > 0 && <Posts posts={posts} />}
+      <Outlet />
+    </div>
+  );
+};
 
 export default Home;
