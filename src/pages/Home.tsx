52edf097b6e278879f2b2200ad17869321899ff6
Ghost <culestfrosh@gmail.com>
2023-06-02 23:36:57 +0100

    userFrom Store

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useAppSelector } from "../app/store";
 import type { USER } from "../app/types";
 import Footer from "../components/Footer";
@@ -10,34 +10,49 @@ import { useAllUsersQuery } from "../features/user/userApiSlice";
 const Home = () => {
   const userFromStore = useAppSelector((state) => state.user.userInfo);
   const [posts, setPosts] = useState<string[]>([]);
-// ts-ignore
-  const { data, error } = useAllUsersQuery(); // Call the hook directly
+  const navigate = useNavigate();
+  const { data, error } = useAllUsersQuery(userFromStore?.token);
 
   useEffect(() => {
-    if (data) {
-      const fetchedPosts: string[] = data.reduce((acc: string[], user: USER) => {
-        if (user.posts) {
-          acc.push(...user.posts);
+   
+   
+    if (userFromStore?.token) {
+    
+      if (data) {
+        const fetchedPosts: string[] = data.users.reduce((acc: string[], user: USER) => {
+          if (user.posts) {
+            acc.push(...user.posts);
+          }
+          return acc;
+        }, []);
+
+        if (fetchedPosts.length > 0) {
+         
+          setPosts(fetchedPosts);
         }
-        return acc;
-      }, []);
-
-      if (fetchedPosts.length > 0) {
-        setPosts(fetchedPosts);
+      } else {
+        toast.error(JSON.stringify(error));
       }
-    } else {
-      toast.error(JSON.stringify(error));
     }
-  }, [data, error]);
+  }, [data, error, userFromStore]);
 
+  useEffect(() => {
+    if (!userFromStore || !userFromStore.token) {
+      navigate("/login");
+    }
+  }, [userFromStore, navigate]);
+if(userFromStore && userFromStore !== null && userFromStore.token){
   return (
     <div>
-    
-      {posts.length > 0 && <Posts posts={posts} />}
-        <Footer user={userFromStore} />
+      {posts.length > 0 && <Posts posts={posts}  token={userFromStore.token}/>}
+      <Footer />
       <Outlet />
     </div>
   );
+}
+return (
+  <h1 onClick={()=>navigate("/login")}>Login</h1>
+  )
 };
 
 export default Home;
