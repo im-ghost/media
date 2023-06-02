6d8da20b319690db250b2521493e339ddc0eff25
Ghost <culestfrosh@gmail.com>
2023-06-02 23:36:34 +0100

    added flex classes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ import Paper from '@mui/material/Paper';
     
 
 
-const Navbar:React.FC<{user:USER | null}> = ({user}):JSX.Element => {
+const Footer:React.FC = ():JSX.Element => {
   const navigate = useNavigate();
   const [value,setValue] = React.useState(0)
 
@@ -35,20 +35,24 @@ const Navbar:React.FC<{user:USER | null}> = ({user}):JSX.Element => {
   
   return (
          <Box sx={{ pb: 7 }} ref={ref} >
-      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3} className="flex w-screen ">
         <BottomNavigation
           showLabels
           value={value}
           onChange={(event, newValue) => {
             setValue(newValue);
           }}
+          className="flex w-full h-8 justify-evenly items-center"
         >
-          <BottomNavigationAction label="Recents" onClick={()=> navigate("/notifications")} icon={<h1>not</h1>} />
-          <BottomNavigationAction label="Favorites" onClick={()=> navigate("/messages")} icon={<h1>mssg</h1>} />
+          <BottomNavigationAction label="Notifications" onClick={()=> navigate("/notifications")} icon={<h1>🔔</h1>} />
+          <BottomNavigationAction label="Messages" onClick={()=> navigate("/messages")} icon={<h1>🗨️</h1>} />
+          <BottomNavigationAction label="Home" onClick={()=> navigate("/")} icon={<h1>🏠</h1>} />
+          <BottomNavigationAction label="Search" onClick={()=> navigate("/search")} icon={<h1>🔍</h1>} />
+          <BottomNavigationAction label="profile" onClick={()=> navigate("/profile")} icon={<h1>👤</h1>} />
         
         </BottomNavigation>
       </Paper>
     </Box>
     )
 }
-export default Navbar
+export default Footer
