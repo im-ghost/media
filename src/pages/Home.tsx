e32715567287cc5b18392298c2de8f9c58ac2049
Ghost <culestfrosh@gmail.com>
2023-06-03 23:02:09 +0100

    changed the classes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,6 +18,9 @@ const Home = () => {
   const navigate = useNavigate();
   const { data, error } = useAllUsersQuery(userFromStore?.token);
 
+useEffect(()=>{
+  toast.info("Welcome to media")
+},[])
   useEffect(() => {
    
    
@@ -48,11 +51,11 @@ const Home = () => {
   }, [userFromStore, navigate]);
 if(userFromStore && userFromStore !== null && userFromStore.token){
   return (
-    <div className="bg-sky-300 text-amber-800 dark:bg-slate-900 dark:text-amber-100">
-    <div className="w-screen h-10 flex items-center text-center justify-center  p-2">
+    <div className="bg p-0 m-0">
+    <div className="w-screen h-10 flex items-center justify-center m-0">
    
-    <img src={Favicon} alt="Media" className="h-8 w-8 rounded-[50%]" />
-    <Typography variant="h3">Media</Typography>
+    <img src={Favicon} alt="Media" className="h-8 w-8 p-0 m-0 rounded-[50%]" />
+    <Typography variant="h3" className="text-bold">Media</Typography>
     </div>
       {posts.length > 0 && <Posts posts={posts}  token={userFromStore.token}/>}
      
