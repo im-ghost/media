9ee209a25496737bdd116af56078def9f42624cb
Richard Adetonwa <culestfrosh@gmail.com>
2023-06-19 18:48:43 +0100

    register

---
+++ src/pages/auth/firebase.ts
@@ -0,0 +1,54 @@
+import { 
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  TwitterAuthPrivider
+} from "firebase/auth";
+
+const auth = getAuth();
+export const google = () => {
+const provider = new GoogleAuthProvider();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    user.token = token;
+    if(user) return user
+    throw new Error("An error Occured")
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    throw new Error("An error Occured")
+  });
+}
+export const twitter = () => {
+const provider = new TwitterAuthProvider();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    user.token = token;
+    if(user) return user
+    throw new Error("An error Occured")
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    throw new Error("An error Occured")
+  });
+}
+export const emailLink = () => {}
+export const phone = () => {}
---
+++ src/pages/auth/reducer.ts
@@ -0,0 +1,86 @@
+
+import type { USER } from "../../app/types"
+
+
+ export const initialState:USER = {
+   name:"",
+   email:"",
+   phone:"",
+   password:"",
+   bio:"",
+   password2:"",
+   image:null
+ }
+ type ACTION = {
+  type: string;
+  payload?: any;
+};
+ export const reducer = (state:USER,action:ACTION):USER =>{
+   switch (action.type) {
+     case 'setName':
+       return{
+         ...state,
+         name:action.payload
+       }
+       //eslint-disable-next-line
+     break;
+   
+     case 'setEmail':
+       return{
+         ...state,
+         email:action.payload
+       }
+       //eslint-disable-next-line
+     break;
+   
+     case 'setPassword':
+       return{
+         ...state,
+         password:action.payload
+       }
+       //eslint-disable-next-line
+     break;
+   
+     case 'setPassword2':
+       return{
+         ...state,
+         password2:action.payload
+       }
+       //eslint-disable-next-line
+     break;
+   
+     case 'setTel':
+       return{
+         ...state,
+         phone:action.payload
+       }
+       //eslint-disable-next-line
+     break;
+   
+     case 'setBio':
+       return{
+         ...state,
+         bio:action.payload
+       }
+       //eslint-disable-next-line
+     break;
+     case 'setImage':
+       return{
+         ...state,
+         image:action.payload
+       }
+       //eslint-disable-next-line
+     break;
+   
+     case 'reset':
+       return{
+         ...initialState
+       }
+       //eslint-disable-next-line
+     break;
+   
+     default:
+     return state
+   }
+ }
+ 
