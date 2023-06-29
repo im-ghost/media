eeeb9ed9c22fad80a452413184b791aedde2c8f0
Richard Adetonwa <culestfrosh@gmail.com>
2023-06-29 20:45:26 +0100

    restructure

--- src/components/regbuttons.tsx
+++ src/components/regbuttons.tsx
@@ -1,19 +1,11 @@
 import Button from "@mui/material/Button"
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import * as React from "react";
-import { useNavigate } from "react-router-dom";
 import  AuthProviders from "./oauthInput"
 import EmailInput from "./emailInput";
-import {
-  useAppDispatch,
-  useAppSelector
-} from "../app/hooks"
 import PhoneNumberInput from "./phoneInput"
 const RegButtons:React.FC = ():JSX.Element =>{ 
-  const dispatch= useAppDispatch();
-  const userSign = useAppSelector(state=>state.user.userSign)
   const [phone,setPhone] = React.useState(false)
   const [phoneBtn,setPhoneBtn] = React.useState(true)
   const [email,setEmail] = React.useState(false)
@@ -21,8 +13,9 @@ const RegButtons:React.FC = ():JSX.Element =>{
   const [oauth,setOauth] = React.useState(true)
   return(
     <Box className="bg">
-    { oauth && <AuthProviders /> }
+    { oauth && <AuthProviders isLogin={false} /> }
     <div className="p-2 m-2">
+    <Typography variant='h6'> OR </Typography>
    { phoneBtn &&  <Button className="" variant="contained" onClick={()=>{
        setPhone(true);
        setEmail(false);
