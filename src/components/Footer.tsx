edda39e6093cf247083f386f7755f56aab2e4941
Richard Adetonwa <culestfrosh@gmail.com>
2023-06-29 20:44:46 +0100

    unused export removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer:React.FC = ():JSX.Element => {
   const ref = React.useRef<HTMLDivElement>(null);
   React.useEffect(() => {
     (ref.current as HTMLDivElement).ownerDocument.body.scrollTop = 0;
-    
+  
   }, [value]);
   
   return (
