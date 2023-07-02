8d6d5ad45d31aa3fbd1d591c55628196dab335cf
Richard <culestfrosh@gmail.com>
2023-07-02 18:12:30 +0100

    js and ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import "./app/firebase.ts"
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -21,6 +20,30 @@ import {
 } from "react-router-dom"
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
+// Import the functions you need from the SDKs you need
+
+// eslint-disable-next-line
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is options
+
+// eslint-disable-next-line
+const firebaseConfig = {
+  apiKey: "AIzaSyBUDXn0ympH2iaY9vzu_79yMdcS8wYw09o",
+  authDomain: "media-app-g.firebaseapp.com",
+  projectId: "media-app-g",
+  storageBucket: "media-app-g.appspot.com",
+  messagingSenderId: "134965322289",
+  appId: "1:134965322289:web:f2ab5e669af5eaafecab10",
+  measurementId: "G-JJZ9PB8M3H"
+};
+
+// Initialize Firebase
+// eslint-disable-next-line
+const app = initializeApp(firebaseConfig);
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 const router = createBrowserRouter(
