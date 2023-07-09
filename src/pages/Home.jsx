8d08c854440412715bf28ede6a1ae533ea35fd5c
Richard <culestfrosh@gmail.com>
2023-07-09 16:43:46 +0100

    update home.jsx
    Added add buttom and a text to display if there is no post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,32 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Typography } from '@mui/material';
-import Favicon from '../images/logo192.png';
 import { toast } from 'react-toastify';
+import Add from "../components/Add";
 import Posts from '../components/posts';
 import { useAllUsersQuery } from '../features/user/userApiSlice';
+import { selectUser } from '../features/user/userSlice';
 const Home = () => {
-  const userFromStore = useSelector((state) => state.user.userInfo);
-  const [data,setData] = useState({});
-  const [error,setError] = useState({})
+  const userFromStore = useSelector(selectUser);
+  const [data, setData] = useState({});
+  const [error, setError] = useState({});
   const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
-  useEffect(() => {
+  React.useLayoutEffect(() => {
     if (!userFromStore || !userFromStore.token) {
       navigate('/login');
-    }else{
-      const { data:dataF, error:errorF } = useAllUsersQuery(userFromStore.token);
-      setData(dataF)
-      setError(errorF)
     }
   }, [userFromStore, navigate]);
-  
+
   useEffect(() => {
     toast.info('Welcome to media');
   }, []);
+
+  const { data: dataF, error: errorF } = useAllUsersQuery(userFromStore.token);
   useEffect(() => {
-    if (userFromStore?.token) {
-      if (data) {
+    if (dataF || errorF) {
+      setData(dataF);
+      setError(errorF);
+      console.log('f' + dataF);
+      console.log('d' + data);
+    }
+    if (errorF) {
+      navigate('/');
+    }
+  }, [dataF, errorF]);
+  useEffect(() => {
+    if (data) {
+      if (data.users) {
+        console.log(data);
         const fetchedPosts = data.users.reduce((acc, user) => {
           if (user.posts) {
             acc.push(...user.posts);
@@ -47,24 +58,24 @@ const Home = () => {
       <div className="bg p-0 m-0">
         <div className="w-screen h-10 flex items-center justify-center m-0">
           <img
-            src={Favicon}
+            src={require("../images/logo192.png")}
             alt="Media"
             className="h-8 w-8 p-0 m-0 rounded-[50%] animate-bounce"
           />
           <Typography
-            variant="h3"
+            variant="h4"
             className="text-bold"
           >
             Media
           </Typography>
         </div>
-        {posts.length > 0 && (
+        {posts.length > 0 ? (
           <Posts
             posts={posts}
             token={userFromStore.token}
           />
-        )}
-
+        ) : <Typography variant="h4">No posts available.</Typography>}
+       <Add />
         <Outlet />
       </div>
     );
