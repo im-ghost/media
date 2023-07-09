6f8a51168d74cd42519a2d40b26507c801acc9cc
Richard <culestfrosh@gmail.com>
2023-07-09 16:51:31 +0100

    allowed all foemer routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,21 @@
+import "./app/firebase.js"
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import Home from './pages/Home'; 
+import Home from './pages/Home';
 import Login from './pages/auth/Login';
-/*
 import Post from './pages/Post';
 import CreatePost from './pages/CreatePost';
-import Logout from './pages/Logout';*/
-//
-//import Register from './pages/auth/Register';
-//import Register2 from './pages/auth/Register2';
-//import Profile from './pages/Profile';
+import Logout from './pages/Logout';
+import Register from './pages/auth/Register';
+import Register2 from './pages/auth/Register2';
+import Profile from './pages/Profile';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
-import { initializeApp } from 'firebase/app';
-const firebaseConfig = {
-  apiKey: 'AIzaSyBUDXn0ympH2iaY9vzu_79yMdcS8wYw09o',
-  authDomain: 'media-app-g.firebaseapp.com',
-  projectId: 'media-app-g',
-  storageBucket: 'media-app-g.appspot.com',
-  messagingSenderId: '134965322289',
-  appId: '1:134965322289:web:f2ab5e669af5eaafecab10',
-  measurementId: 'G-JJZ9PB8M3H',
-};
-// Initialize Firebase
-// eslint-disable-next-line
-const app = initializeApp(firebaseConfig);
 /*const router = createBrowserRouter(
   createRoutesFromElements(
     <Route
@@ -41,7 +27,31 @@ const app = initializeApp(firebaseConfig);
         path="/"
         element={<Home />}
       />
-      
+     
+    </Route>
+  )
+);*/
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<App />}
+          >
+            <Route
+              path="/"
+              index
+              element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+             
       <Route
         path="/logout"
         element={<Logout />}
@@ -65,29 +75,6 @@ const app = initializeApp(firebaseConfig);
       <Route
         path="/posts/:id"
         element={<Post />}
-      />
-    </Route>
-  )
-);*/
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<App />}
-          >
-            <Route
-              path="/"
-              index
-              element={<Home />}
-            />
-            <Route
-        path="/login"
-        element={<Login />}
       />
           </Route>
         </Routes>
