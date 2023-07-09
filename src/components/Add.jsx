4baec75ee7d5be0d3cb0481e514f75241a5f1460
Richard <culestfrosh@gmail.com>
2023-07-09 22:56:40 +0100

    prettier

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Fab,Box } from '@mui/material';
+import { Fab, Box } from '@mui/material';
 import { MdAdd } from 'react-icons/md';
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
 const Add = () => {
-  const navigate= useNavigate()
+  const navigate = useNavigate();
   return (
     <Box
       style={{
         position: 'fixed',
         bottom: '5em',
         right: '3em',
-        zIndex: '9999', 
+        zIndex: '9999',
       }}
       className=""
-      onClick={()=>navigate("/createpost")}
+      onClick={() => navigate('/createpost')}
     >
-<Fab color="primary">
-  <MdAdd className="text-bold text-3xl" />
-</Fab>
+      <Fab color="primary">
+        <MdAdd className="text-bold text-3xl" />
+      </Fab>
     </Box>
   );
 };
 
 export default Add;
-
