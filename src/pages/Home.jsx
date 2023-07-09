8af1c3c5717042418530e1cf440d1b83c2fec56a
Richard <culestfrosh@gmail.com>
2023-07-09 22:56:12 +0100

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,62 +3,24 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Typography } from '@mui/material';
 import { toast } from 'react-toastify';
-import Add from "../components/Add";
-import Posts from '../components/posts';
-import { useAllUsersQuery } from '../features/user/userApiSlice';
+import Add from '../components/Add';
 import { selectUser } from '../features/user/userSlice';
+import Helper from '../components/home';
 const Home = () => {
   const userFromStore = useSelector(selectUser);
-  const [data, setData] = useState({});
-  const [error, setError] = useState({});
-  const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
-  React.useLayoutEffect(() => {
-    if (!userFromStore || !userFromStore.token) {
+  useEffect(() => {
+    if (userFromStore === null) {
       navigate('/login');
     }
   }, [userFromStore, navigate]);
 
-  useEffect(() => {
-    toast.info('Welcome to media');
-  }, []);
-
-  const { data: dataF, error: errorF } = useAllUsersQuery(userFromStore.token);
-  useEffect(() => {
-    if (dataF || errorF) {
-      setData(dataF);
-      setError(errorF);
-      console.log('f' + dataF);
-      console.log('d' + data);
-    }
-    if (errorF) {
-      navigate('/');
-    }
-  }, [dataF, errorF]);
-  useEffect(() => {
-    if (data) {
-      if (data.users) {
-        console.log(data);
-        const fetchedPosts = data.users.reduce((acc, user) => {
-          if (user.posts) {
-            acc.push(...user.posts);
-          }
-          return acc;
-        }, []);
-        if (fetchedPosts.length > 0) {
-          setPosts(fetchedPosts);
-        }
-      } else {
-        toast.error(JSON.stringify(error));
-      }
-    }
-  }, [data, error, userFromStore]);
-  if (userFromStore && userFromStore !== null && userFromStore.token) {
+  if (userFromStore && userFromStore !== null) {
     return (
       <div className="bg p-0 m-0">
         <div className="w-screen h-10 flex items-center justify-center m-0">
           <img
-            src={require("../images/logo192.png")}
+            src={require('../images/logo192.png')}
             alt="Media"
             className="h-8 w-8 p-0 m-0 rounded-[50%] animate-bounce"
           />
@@ -69,13 +31,11 @@ const Home = () => {
             Media
           </Typography>
         </div>
-        {posts.length > 0 ? (
-          <Posts
-            posts={posts}
-            token={userFromStore.token}
-          />
-        ) : <Typography variant="h4">No posts available.</Typography>}
-       <Add />
+        <Helper
+          userFromStore={userFromStore}
+          token={userFromStore.token}
+        />
+        <Add />
         <Outlet />
       </div>
     );
