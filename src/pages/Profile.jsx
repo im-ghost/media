012fda0fecfee8d4f72b56e4675c4229f1ff2b94
Richard <culestfrosh@gmail.com>
2023-07-09 22:54:51 +0100

    Added helmet

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,9 +12,9 @@ import {
   Tab,
   AppBar,
 } from '@mui/material';
-import { Helmet } from "react-helmet"
+import { Helmet } from 'react-helmet';
 import React, { useEffect, useState, useLayoutEffect } from 'react';
-import { useSelector } from "react-redux"
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import SwipeableViews from 'react-swipeable-views';
 import { useTheme } from '@mui/material/styles';
@@ -80,154 +80,158 @@ const Profile = () => {
   }
   return (
     <>
-    <Helmet>
-    <title> {user.name} </title>
-    
-    <meta
-      name="description"
-      content={user.bio}
-    />
-    <meta
-      property="og:image"
-      content={ user.image ? user.image : require("../images/default.png")}
-    />
-    </Helmet>
-    <Container className="w-screen h-screen py-2 rounded flex items-center justify-center">
-      {user && (
-        <div className="w-[98vw] h-[98vh] p-2 rounded flex items-center justify-center">
-          {user !== null && (
-            <Container className="w-[100vw] h-[100vh]">
-              <Card className="w-full">
-                <CardMedia
-                  image={user.image ? user.image : require("../images/default.png")}
-                  className="h-0 pt-[56.25%] mt-[30]"
-                  sx={{
-                    height: 0,
-                    paddingTop: '56.25%',
-                    marginTop: '30',
-                  }}
-                />
-                <CardContent className="w-full text-center">
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                  >
-                    {user.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                  >
-                    {user.bio}
-                  </Typography>
-                </CardContent>
-                <CardActions className="w-full flex justify-between flex-row">
-                  <Button
-                    variant="contained"
-                    size="small"
-                  >
-                    Edit Profile
-                  </Button>
-                  <Button
-                    variant="contained"
-                    size="small"
-                  >
-                    Delete Account
-                  </Button>
-                </CardActions>
-              </Card>
-              <Paper
-                variant="elevation"
-                className="flex flex-wrap items-center justify-evenly h-[20vh] w-full"
-              >
-                <Box className="flex flex-col flex-wrap items-center justify-center">
-                  <Typography variant="h6">Posts</Typography>
-                  {user.posts && (
-                    <Typography variant="body2">{user.posts.length}</Typography>
-                  )}
-                </Box>
-                <Box className="flex flex-col flex-wrap items-center justify-center">
-                  <Typography variant="h6">Followers</Typography>
-                  {user.followers && (
-                    <Typography variant="body2">
-                      {user.followers.length}
+      <Helmet>
+        <title> {user.name} </title>
+
+        <meta
+          name="description"
+          content={user.bio}
+        />
+        <meta
+          property="og:image"
+          content={user.image ? user.image : require('../images/default.png')}
+        />
+      </Helmet>
+      <Container className="w-screen h-screen py-2 rounded flex items-center justify-center">
+        {user && (
+          <div className="w-[98vw] h-[98vh] p-2 rounded flex items-center justify-center">
+            {user !== null && (
+              <Container className="w-[100vw] h-[100vh]">
+                <Card className="w-full">
+                  <CardMedia
+                    image={
+                      user.image ? user.image : require('../images/default.png')
+                    }
+                    className="h-0 pt-[56.25%] mt-[30]"
+                    sx={{
+                      height: 0,
+                      paddingTop: '56.25%',
+                      marginTop: '30',
+                    }}
+                  />
+                  <CardContent className="w-full text-center">
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                    >
+                      {user.name}
                     </Typography>
-                  )}
-                </Box>
-                <Box className="flex flex-col flex-wrap items-center justify-center">
-                  <Typography variant="h6">Following</Typography>
-                  {user.following && (
-                    <Typography variant="body2">
-                      {user.following.length}
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                    >
+                      {user.bio}
                     </Typography>
-                  )}
-                </Box>
-              </Paper>
-              {!user.posts && <h1>No posts</h1>}
-              {user.posts && user.token && (
-                <Box className="w-full h-auto min-h-64 border">
-                  <AppBar position="static">
-                    <Tabs
-                      value={value}
-                      onChange={handleChange}
-                      indicatorColor="secondary"
-                      textColor="inherit"
-                      variant="fullWidth"
-                      aria-label="full width tabs example"
+                  </CardContent>
+                  <CardActions className="w-full flex justify-between flex-row">
+                    <Button
+                      variant="contained"
+                      size="small"
                     >
-                      <Tab
-                        label="Posts"
-                        {...a11yProps(0)}
-                      />
-                      <Tab
-                        label="Images"
-                        {...a11yProps(1)}
-                      />
-                      <Tab
-                        label="Vidoes"
-                        {...a11yProps(2)}
-                      />
-                    </Tabs>
-                  </AppBar>
-                  <SwipeableViews
-                    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                    className="h-auto min-h-64 "
-                    index={value}
-                    onChangeIndex={handleChangeIndex}
-                  >
-                    <TabPanel
-                      value={value}
-                      index={0}
-                      dir={theme.direction}
+                      Edit Profile
+                    </Button>
+                    <Button
+                      variant="contained"
+                      size="small"
                     >
-                      <Posts
-                        posts={user.posts}
-                        token={user.token}
-                      />
-                    </TabPanel>
-                    <TabPanel
-                      value={value}
-                      index={1}
-                      dir={theme.direction}
+                      Delete Account
+                    </Button>
+                  </CardActions>
+                </Card>
+                <Paper
+                  variant="elevation"
+                  className="flex flex-wrap items-center justify-evenly h-[20vh] w-full"
+                >
+                  <Box className="flex flex-col flex-wrap items-center justify-center">
+                    <Typography variant="h6">Posts</Typography>
+                    {user.posts && (
+                      <Typography variant="body2">
+                        {user.posts.length}
+                      </Typography>
+                    )}
+                  </Box>
+                  <Box className="flex flex-col flex-wrap items-center justify-center">
+                    <Typography variant="h6">Followers</Typography>
+                    {user.followers && (
+                      <Typography variant="body2">
+                        {user.followers.length}
+                      </Typography>
+                    )}
+                  </Box>
+                  <Box className="flex flex-col flex-wrap items-center justify-center">
+                    <Typography variant="h6">Following</Typography>
+                    {user.following && (
+                      <Typography variant="body2">
+                        {user.following.length}
+                      </Typography>
+                    )}
+                  </Box>
+                </Paper>
+                {!user.posts && <h1>No posts</h1>}
+                {user.posts && user.token && (
+                  <Box className="w-full h-auto min-h-64 border">
+                    <AppBar position="static">
+                      <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="secondary"
+                        textColor="inherit"
+                        variant="fullWidth"
+                        aria-label="full width tabs example"
+                      >
+                        <Tab
+                          label="Posts"
+                          {...a11yProps(0)}
+                        />
+                        <Tab
+                          label="Images"
+                          {...a11yProps(1)}
+                        />
+                        <Tab
+                          label="Vidoes"
+                          {...a11yProps(2)}
+                        />
+                      </Tabs>
+                    </AppBar>
+                    <SwipeableViews
+                      axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                      className="h-auto min-h-64 "
+                      index={value}
+                      onChangeIndex={handleChangeIndex}
                     >
-                      videos
-                    </TabPanel>
-                    <TabPanel
-                      value={value}
-                      index={2}
-                      dir={theme.direction}
-                    >
-                      images
-                    </TabPanel>
-                  </SwipeableViews>
-                </Box>
-              )}
-            </Container>
-          )}
-        </div>
-      )}
-    </Container>
+                      <TabPanel
+                        value={value}
+                        index={0}
+                        dir={theme.direction}
+                      >
+                        <Posts
+                          posts={user.posts}
+                          token={user.token}
+                        />
+                      </TabPanel>
+                      <TabPanel
+                        value={value}
+                        index={1}
+                        dir={theme.direction}
+                      >
+                        videos
+                      </TabPanel>
+                      <TabPanel
+                        value={value}
+                        index={2}
+                        dir={theme.direction}
+                      >
+                        images
+                      </TabPanel>
+                    </SwipeableViews>
+                  </Box>
+                )}
+              </Container>
+            )}
+          </div>
+        )}
+      </Container>
     </>
   );
 };
