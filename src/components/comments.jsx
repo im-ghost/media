0abcfcf37b88701887ac4471548c8da9e4d8af2e
Richard <culestfrosh@gmail.com>
2023-07-11 15:32:45 +0100

    comments component

---
+++ src/components/comments.jsx
@@ -0,0 +1,18 @@
+import Comment from './comment';
+import React from "react"
+import { Container } from '@mui/material';
+const Comments = ({ comments, token, user }) => {
+  return (
+    <Container className="p-2">
+      {comments.map((comment) => (
+        <Comment
+          comment={comment}
+          token={token}
+          user={user}
+        />
+      ))}
+    </Container>
+  );
+};
+
+export default Comments;
