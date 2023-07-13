3173531642ece218df3274a30f50484cfad29a61
Richard <culestfrosh@gmail.com>
2023-07-13 08:08:31 +0100

    prettier

--- src/components/chelper.jsx
+++ src/components/chelper.jsx
@@ -1,26 +1,21 @@
-import { useGetUserByIdQuery } from "../features/user/userApiSlice"
-import {
-  Typography
-} from "@mui/material"
-import React,{
-  useState,
-  useEffect
-} from "react"
-export default function Helper({authorId}){
-  const [author,setAuthor] = useState(null)
-  const {data,error} = useGetUserByIdQuery(authorId);
-  useEffect(()=>{
-    if(data){
-      setAuthor(data.user)
+import { useGetUserByIdQuery } from '../features/user/userApiSlice';
+import { Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+export default function Helper({ authorId }) {
+  const [author, setAuthor] = useState(null);
+  const { data, error } = useGetUserByIdQuery(authorId);
+  useEffect(() => {
+    if (data) {
+      setAuthor(data.user);
       console.log(data.user);
     }
-    if(error){
-      toast.error(JSON.stringify(error))
+    if (error) {
+      toast.error(JSON.stringify(error));
     }
-  },[data,error])
-  if(author){
-  return(
-    <div
+  }, [data, error]);
+  if (author) {
+    return (
+      <div
         className="flex w-3/4 items-center"
         onClick={() => navigate(`/users/${author._id}`)}
       >
@@ -30,10 +25,14 @@ export default function Helper({authorId}){
           className="h-6 w-auto rounded-[50%] mr-2"
         />
 
-        <Typography variant="body2" className="flex-grow-2 whitespace-nowrap">{author.name}</Typography>
-          
+        <Typography
+          variant="body2"
+          className="flex-grow-2 whitespace-nowrap"
+        >
+          {author.name}
+        </Typography>
       </div>
-    )
+    );
   }
-  return <Typography variant="body2">Loading...</Typography>
+  return <Typography variant="body2">Loading...</Typography>;
 }
