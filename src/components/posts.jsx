98b209bfb253ef7fe3a688be6d18d266f83bd619
Richard <culestfrosh@gmail.com>
2023-07-13 15:33:13 +0100

    prettier

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -3,62 +3,64 @@ import { Container, Typography } from '@mui/material';
 import Post from './post';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { useSelector,useDispatch } from "react-redux"
-import { 
+import { useSelector, useDispatch } from 'react-redux';
+import {
   setPostsInStore,
   setPostsObjInStore,
-  selectPostsObj
+  selectPostsObj,
 } from '../features/post/postSlice';
 const Posts = ({ posts, token }) => {
   const [postsObj, setPosts] = useState(null);
-  const postsFromStore = useSelector(selectPostsObj)
+  const postsFromStore = useSelector(selectPostsObj);
   const dispatch = useDispatch();
   useEffect(() => {
-    if(postsFromStore){
-      setPosts(postsFromStore)
-    }else{
-    const fetchPosts = async () => {
-      try {
-        let postIds = posts.filter((post) => post !== null); // Filter out null values
-        postIds = posts.filter((post) => post !== undefined); // Filter out undefined values
-        if (postIds.length > 0) {
-          const resolvedPosts = await Promise.all(
-            postIds.map(async (postId) => {
-              try {
-                const { data } = await axios.get(
-                  `http://localhost:4000/api/v1/posts/post/${postId}`,
-                  {
-                    headers: {
-                      authorization: token,
-                    },
+    if (postsFromStore) {
+      console.log('str', postsFromStore);
+      setPosts(postsFromStore);
+    } else {
+      const fetchPosts = async () => {
+        try {
+          let postIds = posts.filter((post) => post !== null); // Filter out null values
+          postIds = posts.filter((post) => post !== undefined); // Filter out undefined values
+          if (postIds.length > 0) {
+            const resolvedPosts = await Promise.all(
+              postIds.map(async (postId) => {
+                try {
+                  const { data } = await axios.get(
+                    `http://localhost:4000/api/v1/posts/post/${postId}`,
+                    {
+                      headers: {
+                        authorization: token,
+                      },
+                    }
+                  );
+                  if (data) return data;
+                  else {
+                    toast.error('No post available');
+                    return undefined; // Return undefined when data is not available
                   }
-                );
-                if (data) return data;
-                else {
-                  toast.error('No post available');
-                  return undefined; // Return undefined when data is not available
+                } catch (error) {
+                  toast.error(JSON.stringify(error));
+                  return undefined; // Return undefined in case of an error
                 }
-              } catch (error) {
-                toast.error(JSON.stringify(error));
-                return undefined; // Return undefined in case of an error
-              }
-            })
-          );
-          // Filter out undefined values from the resolvedPosts array
-          const filteredPosts = resolvedPosts.filter(
-            (post) => post !== undefined
-          );
-          setPosts(filteredPosts);
-          dispatch(setPostsObjInStore(filteredPosts))
-          return filteredPosts; // Return the filtered and resolved posts
+              })
+            );
+            // Filter out undefined values from the resolvedPosts array
+            const filteredPosts = resolvedPosts.filter(
+              (post) => post !== undefined
+            );
+            console.log('fi', filteredPosts);
+            setPosts(filteredPosts);
+            dispatch(setPostsObjInStore(filteredPosts));
+            return filteredPosts; // Return the filtered and resolved posts
+          }
+        } catch (error) {
+          toast.error(JSON.stringify(error));
         }
-      } catch (error) {
-        toast.error(JSON.stringify(error));
+      };
+      if (posts) {
+        fetchPosts();
       }
-    };
-    if (posts) {
-      fetchPosts();
-    }
     }
   }, [posts, dispatch, token]);
   if (posts.length < 1) {
