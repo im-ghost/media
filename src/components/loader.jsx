284ff4816314c1a232fb64ccd3501b63984dbed4
Richard <culestfrosh@gmail.com>
2023-07-13 15:33:24 +0100

    loader

---
+++ src/components/loader.jsx
@@ -0,0 +1,7 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+const Loader = () => {
+  return <Typography variant="h1">Loading...</Typography>;
+};
+
+export default Loader;
