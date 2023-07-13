2c6b20cbedef46d03f5436306cfddffb950adbf1
Richard <culestfrosh@gmail.com>
2023-07-13 08:03:20 +0100

    added postObj and comments to store

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  posts: null,
+  posts: undefined,
   status: 'idle',
+  postsObj:undefined,
+  comments:undefined,
 };
 export const postSlice = createSlice({
   name: 'post',
@@ -14,8 +16,33 @@ export const postSlice = createSlice({
       console.log('saved');
       console.log(state.posts);
     },
+    setPostsObjInStore: (state, action) => {
+      state.postsObj = action.payload;
+     
+    },
+    setCommentsInStore: (state, action) => {
+      state.comments = action.payload;
+    },
+    addToStorePosts: (state, action) => {
+      const posts = state.posts;
+      state.posts = posts.push(action.payload);
+    },
+    updatePosts: (state, action) => {
+      const {
+        id
+      } = action.payload;
+      const dPost = state.posts.find(post => post._id.toString() === id.toString());
+      if(dPost){
+        const newPosts = state.posts.filter(post=> post._id.toString() === id.toString());
+        const newPosts2 = newPosts.push(action.payload);
+        state.posts = newPosts2
+      }
+      
+    },
   },
 });
-export const { setPostsInStore } = postSlice.actions;
+export const { setPostsInStore,setPostsObjInStore,setCommentsInStore,addToStorePosts,updatePosts } = postSlice.actions;
 export const selectPosts = (state) => state.post.posts;
+export const selectComments = (state) => state.post.comments;
+export const selectPostsObj = (state) => state.post.postsObj;
 export default postSlice.reducer;
