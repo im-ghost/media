ff81f35266d793d866e060792b069ad29c912700
Richard <culestfrosh@gmail.com>
2023-07-13 08:05:18 +0100

    update the store after a user creates a post

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,13 +1,17 @@
 import { Typography, Box, Button, TextField } from '@mui/material';
 import * as React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from "react-redux"
+import { 
+  updatePosts
+} from '../features/post/postSlice';
 import { useCreatePostMutation } from '../features/post/postApiSlice';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 const CreatePost = () => {
   const [user, setUser] = React.useState(null);
   const [text, setText] = React.useState('');
+  const dispatch= useDispatch()
   const userInfo = useSelector((state) => state.user.userInfo);
   const navigate = useNavigate();
   const [createPost, { error, data }] = useCreatePostMutation();
@@ -28,6 +32,7 @@ const CreatePost = () => {
   }, [error]);
   React.useEffect(() => {
     if (data) {
+      dispatch(updatePosts(data.post))
       navigate('/');
     }
   }, [data, navigate]);
