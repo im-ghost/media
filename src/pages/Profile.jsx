b14fe0fce69c78ad7780c7322d14bdfc4f4eb5a7
Richard <culestfrosh@gmail.com>
2023-07-13 08:04:38 +0100

    Added user details fetching,incase user already created posts between logins

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,6 +12,7 @@ import {
   Tab,
   AppBar,
 } from '@mui/material';
+import { useGetUserByIdQuery } from '../features/user/userApiSlice';
 import { Helmet } from 'react-helmet';
 import React, { useEffect, useState, useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -46,24 +47,12 @@ function a11yProps(index) {
 const Profile = () => {
   const navigate = useNavigate();
   const userFromStore = useSelector((state) => state.user.userInfo);
-  const [user, setUser] = useState(userFromStore);
+  useEffect(()=>{
+    if(!userFromStore) navigate("/login")
+  },[userFromStore])
+  const [user, setUser] = useState();
   const [value, setValue] = useState(0);
-  useLayoutEffect(() => {
-    if (userFromStore) {
-      setUser(userFromStore);
-    } else {
-      navigate('/login');
-    }
-  }, [userFromStore, navigate]);
-  useEffect(() => {
-    if (userFromStore) {
-      setUser(userFromStore);
-    }
-    if (!user) {
-      navigate('/login');
-    }
-    console.log(user);
-  }, [userFromStore, navigate, user]);
+
   const theme = useTheme();
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -71,6 +60,17 @@ const Profile = () => {
   const handleChangeIndex = (index) => {
     setValue(index);
   };
+  
+  const { data, error } = useGetUserByIdQuery(userFromStore._id);
+  useEffect(() => {
+    if (data) {
+      setUser(data.user);
+      console.log(data.user);
+    }
+    if (error) {
+      toast.error(JSON.stringify(error));
+    }
+  }, [data, error]);
   if (!user) {
     return (
       <Card onClick={() => navigate('/login')}>
@@ -92,145 +92,118 @@ const Profile = () => {
           content={user.image ? user.image : require('../images/default.png')}
         />
       </Helmet>
-      <Container className="w-screen h-screen py-2 rounded flex items-center justify-center">
-        {user && (
-          <div className="w-[98vw] h-[98vh] p-2 rounded flex items-center justify-center">
-            {user !== null && (
-              <Container className="w-[100vw] h-[100vh]">
-                <Card className="w-full">
-                  <CardMedia
-                    image={
-                      user.image ? user.image : require('../images/default.png')
-                    }
-                    className="h-0 pt-[56.25%] mt-[30]"
-                    sx={{
-                      height: 0,
-                      paddingTop: '56.25%',
-                      marginTop: '30',
-                    }}
-                  />
-                  <CardContent className="w-full text-center">
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                    >
-                      {user.name}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                    >
-                      {user.bio}
-                    </Typography>
-                  </CardContent>
-                  <CardActions className="w-full flex justify-between flex-row">
-                    <Button
-                      variant="contained"
-                      size="small"
-                    >
-                      Edit Profile
-                    </Button>
-                    <Button
-                      variant="contained"
-                      size="small"
-                    >
-                      Delete Account
-                    </Button>
-                  </CardActions>
-                </Card>
-                <Paper
-                  variant="elevation"
-                  className="flex flex-wrap items-center justify-evenly h-[20vh] w-full"
-                >
-                  <Box className="flex flex-col flex-wrap items-center justify-center">
-                    <Typography variant="h6">Posts</Typography>
-                    {user.posts && (
-                      <Typography variant="body2">
-                        {user.posts.length}
-                      </Typography>
-                    )}
-                  </Box>
-                  <Box className="flex flex-col flex-wrap items-center justify-center">
-                    <Typography variant="h6">Followers</Typography>
-                    {user.followers && (
-                      <Typography variant="body2">
-                        {user.followers.length}
-                      </Typography>
-                    )}
-                  </Box>
-                  <Box className="flex flex-col flex-wrap items-center justify-center">
-                    <Typography variant="h6">Following</Typography>
-                    {user.following && (
-                      <Typography variant="body2">
-                        {user.following.length}
-                      </Typography>
-                    )}
-                  </Box>
-                </Paper>
-                {!user.posts && <h1>No posts</h1>}
-                {user.posts && user.token && (
-                  <Box className="w-full h-auto min-h-64 border">
-                    <AppBar position="static">
-                      <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        indicatorColor="secondary"
-                        textColor="inherit"
-                        variant="fullWidth"
-                        aria-label="full width tabs example"
-                      >
-                        <Tab
-                          label="Posts"
-                          {...a11yProps(0)}
-                        />
-                        <Tab
-                          label="Images"
-                          {...a11yProps(1)}
-                        />
-                        <Tab
-                          label="Vidoes"
-                          {...a11yProps(2)}
-                        />
-                      </Tabs>
-                    </AppBar>
-                    <SwipeableViews
-                      axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                      className="h-auto min-h-64 "
-                      index={value}
-                      onChangeIndex={handleChangeIndex}
-                    >
-                      <TabPanel
-                        value={value}
-                        index={0}
-                        dir={theme.direction}
-                      >
-                        <Posts
-                          posts={user.posts}
-                          token={user.token}
-                        />
-                      </TabPanel>
-                      <TabPanel
-                        value={value}
-                        index={1}
-                        dir={theme.direction}
-                      >
-                        videos
-                      </TabPanel>
-                      <TabPanel
-                        value={value}
-                        index={2}
-                        dir={theme.direction}
-                      >
-                        images
-                      </TabPanel>
-                    </SwipeableViews>
-                  </Box>
-                )}
-              </Container>
-            )}
-          </div>
-        )}
+      <Container className="w-screen h-screen rounded flex items-center justify-center">
+        <Container className="w-[100vw] h-[100vh]">
+          <Card className="w-full">
+            <CardMedia
+              image={user.image ? user.image : require('../images/default.png')}
+              className="h-0 pt-[56.25%] mt-[30]"
+              sx={{
+                height: 0,
+                paddingTop: '56.25%',
+                marginTop: '30',
+              }}
+            />
+            <CardContent className="w-full text-center">
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+              >
+                {user.name}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+              >
+                {user.bio}
+              </Typography>
+            </CardContent>
+            <CardActions className="w-full flex justify-between flex-row">
+              <Button
+                variant="contained"
+                size="small"
+              >
+                Edit Profile
+              </Button>
+              <Button
+                variant="contained"
+                size="small"
+              >
+                Delete Account
+              </Button>
+            </CardActions>
+          </Card>
+          <Paper
+            variant="elevation"
+            className="flex flex-wrap items-center justify-evenly h-[20vh] w-full"
+          >
+            <Box className="flex flex-col flex-wrap items-center justify-center">
+              <Typography variant="h6">Posts</Typography>
+
+              <Typography variant="body2">{user.posts.length}</Typography>
+            </Box>
+            <Box className="flex flex-col flex-wrap items-center justify-center">
+              <Typography variant="h6">Followers</Typography>
+
+              <Typography variant="body2">{user.followers.length}</Typography>
+            </Box>
+            <Box className="flex flex-col flex-wrap items-center justify-center">
+              <Typography variant="h6">Following</Typography>
+              {user.following && (
+                <Typography variant="body2">{user.following.length}</Typography>
+              )}
+            </Box>
+          </Paper>
+          <Box className="w-full h-auto min-h-64 border">
+            <AppBar position="static">
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="secondary"
+                textColor="inherit"
+                variant="fullWidth"
+                aria-label="full width tabs example"
+              >
+                <Tab
+                  label="Posts"
+                  {...a11yProps(0)}
+                />
+                <Tab
+                  label="Retweets"
+                  {...a11yProps(1)}
+                />
+              </Tabs>
+            </AppBar>
+            <SwipeableViews
+              axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+              className="h-auto min-h-64 "
+              index={value}
+              onChangeIndex={handleChangeIndex}
+            >
+              <TabPanel
+                value={value}
+                index={0}
+                dir={theme.direction}
+              >
+                <Posts
+                  posts={user.posts}
+                  token={user.token}
+                />
+              </TabPanel>
+              <TabPanel
+                value={value}
+                index={1}
+                dir={theme.direction}
+              >
+                <Posts
+                  posts={user.retweets}
+                  token={user.token}
+                />
+              </TabPanel>
+            </SwipeableViews>
+          </Box>
+        </Container>
       </Container>
     </>
   );
