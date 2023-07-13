0a86fec408ffaa19b9ad64eba74b3b6ec6636e46
Richard <culestfrosh@gmail.com>
2023-07-13 15:29:41 +0100

    prettier

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -9,6 +9,6 @@ const firebaseConfig = {
   appId: '1:134965322289:web:f2ab5e669af5eaafecab10',
   measurementId: 'G-JJZ9PB8M3H',
 };
-  // Initialize Firebase
-  // eslint-disable-next-line
+// Initialize Firebase
+// eslint-disable-next-line
 const app = initializeApp(firebaseConfig);
