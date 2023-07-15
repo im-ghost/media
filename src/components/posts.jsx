6830619d795055eaae29b57641ec46320ad5ba5c
Richard <culestfrosh@gmail.com>
2023-07-15 23:51:11 +0100

    removed individual fetching of posts

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -15,7 +15,6 @@ const Posts = ({ posts, token }) => {
   const dispatch = useDispatch();
   useEffect(() => {
     if (postsFromStore) {
-      console.log('str', postsFromStore);
       setPosts(postsFromStore);
     } else if (typeof posts[0] === 'object') {
       const dPosts = [];
@@ -25,50 +24,6 @@ const Posts = ({ posts, token }) => {
         });
       });
       setPosts(dPosts);
-    } else {
-      const fetchPosts = async () => {
-        try {
-          let postIds = posts.filter((post) => post !== null); // Filter out null values
-          postIds = posts.filter((post) => post !== undefined); // Filter out undefined values
-          if (postIds.length > 0) {
-            const resolvedPosts = await Promise.all(
-              postIds.map(async (postId) => {
-                try {
-                  const { data } = await axios.get(
-                    `http://localhost:4000/api/v1/posts/post/${postId}`,
-                    {
-                      headers: {
-                        authorization: token,
-                      },
-                    }
-                  );
-                  if (data) return data;
-                  else {
-                    toast.error('No post available');
-                    return undefined; // Return undefined when data is not available
-                  }
-                } catch (error) {
-                  toast.error(JSON.stringify(error));
-                  return undefined; // Return undefined in case of an error
-                }
-              })
-            );
-            // Filter out undefined values from the resolvedPosts array
-            const filteredPosts = resolvedPosts.filter(
-              (post) => post !== undefined
-            );
-            console.log('fi', filteredPosts);
-            setPosts(filteredPosts);
-            dispatch(setPostsObjInStore(filteredPosts));
-            return filteredPosts; // Return the filtered and resolved posts
-          }
-        } catch (error) {
-          toast.error(JSON.stringify(error));
-        }
-      };
-      if (posts) {
-        fetchPosts();
-      }
     }
   }, [posts, dispatch, token]);
   if (posts.length < 1) {
