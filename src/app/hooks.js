2acee3e21b6b2f6f24b40537a35a884f5c010a74
Richard <culestfrosh@gmail.com>
2023-07-15 23:47:22 +0100

    usenotify util

--- src/app/hooks.js
+++ src/app/hooks.js
@@ -5,17 +5,22 @@ export const useNotify = async ({
   content, userId, token, authorId,
 }) => {
   if (authorId.toString() === userId.toString()) {
+    console.log('usenotify', token);
+    const url = `http://localhost:4000/api/v1/not/n/${userId}`;
     try {
-      const {
-        data,
-      } = await axios.post(`http://localhost:4000/users/user/${userId}/notifications`, {
+      const res = await axios({
+        method: 'post',
+        url,
         headers: {
           authorization: token,
         },
-        body: content,
+        data: {
+          content,
+        },
       });
-      if (data) {
-        toast.info(JSON.stringify(data));
+      if (res) {
+        console.log(res.data);
+        toast.info(JSON.stringify(res.data.notification.content));
       }
     } catch (e) {
       toast.error(JSON.stringify(e));
