1d178c745633493bbfa1fac5bc3bebbd09d7d42f
Richard <culestfrosh@gmail.com>
2023-07-18 19:07:58 +0100

    navigating to login page if user equals null and page isnot an auth page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/user/userslice";
 import Footer from './components/Footer';
 function App() {
   const location = useLocation();
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+  React.useEffect(()=>{
   const hideFooterRoutes = ['/login', '/register', '/register2'];
   const shouldHideFooter = hideFooterRoutes.some((route) =>
     location.pathname.includes(route)
   );
+    if(!shouldHideFooter){
+      if(!user){
+        navigate("/login")
+      }
+    }
+  },[location])
   return (
     <div className="dark">
       <ToastContainer />
