1293d56d440a290d3cba5969b6848c5f23aa95eb
Richard <culestfrosh@gmail.com>
2023-07-18 20:30:26 +0100

    message component

---
+++ src/components/message.jsx
@@ -0,0 +1,11 @@
+import {} from "@mui/material";
+import React,{} from "react";
+
+export default function Message({message,isUsers}) {
+  return (
+    <Box className={`p-2 rounded m-2   ${isUsers ? "justify-self-start" : "justify-self-end"}`}>
+     <Typography variant="body2">{message.timestamp}</Typography>
+     <Typography variant="body1">{message.message}</Typography>
+    </Box>
+    )
+}
