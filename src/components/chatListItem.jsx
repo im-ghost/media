87125bee407c8754c429bd4bb9dad4156ac681ef
Richard <culestfrosh@gmail.com>
2023-07-18 20:22:47 +0100

    a chat list item

---
+++ src/components/chatListItem.jsx
@@ -0,0 +1,37 @@
+import {
+  Paper,
+  Box,
+  Badge,
+  Typography
+} from "@mui/material";
+import { useNavigate } from "react-router-dom"
+import React,{} from "react"
+
+export default function chatListItem({chat,user}) {
+  const { reciever,messages } = chat;
+  const unread = 0;
+  useEffect(()=>{
+    for (let i = 1; i < messages.length; i++) {
+      if(messages[messages.length-i].read){
+        break;
+      }else{
+        unread++
+      }
+      
+    }
+  },[messages])
+  const navigate= useNavigate();
+  return(
+    <Paper className="">
+     <img src={reciever.image} className="rouded-[50%] h-8 w-8"/>
+     <Box className="">
+      <Typography variant="h6">{reciever.name}</Typography>
+      <Typography variant="body1">{messages[0]. message}</Typography>
+     </Box>
+     <Box className="">
+     <Typography variant="body2">{messages[0].timestamp}</Typography>
+     <Badge badgeContent={unread}></Badge>
+     </Box>
+    </Paper>
+    )
+}
