05eeedf04c8955be0cb173fc66e56ed82a836089
Richard <culestfrosh@gmail.com>
2023-07-18 19:08:38 +0100

    added chat reducer"

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { io } from 'socket.io-client';
 import userReducer from '../features/user/userSlice';
 import postReducer from '../features/post/postSlice';
+import chatReducer from '../features/chat/chatSlice';
 import apiSlice from './api';
 
 export const socket = io('http://localhost:4000');
@@ -9,6 +10,7 @@ export const store = configureStore({
   reducer: {
     user: userReducer,
     post: postReducer,
+    chat: chatReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
