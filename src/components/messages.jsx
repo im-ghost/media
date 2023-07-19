c708dba61b8ea3d786039e706c89a306474808de
Richard <culestfrosh@gmail.com>
2023-07-19 18:18:36 +0100

    messages container

---
+++ src/components/messages.jsx
@@ -0,0 +1,13 @@
+import {
+  Box
+} from "@mui/material";
+import React from "react";
+import Message from "./message"
+const Messages = ({ messages,user }) => {
+  return (
+    <Box className="">
+    { messages.map((message)=> <Message message={message} isUsers{message.sender === user._id}/>) }
+    </Box>
+    )
+}
+export default Messages;
