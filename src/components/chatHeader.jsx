711490a5bf49384407a018eb1c81d1dad1a7da82
Richard <culestfrosh@gmail.com>
2023-07-19 18:19:07 +0100

    chat header

---
+++ src/components/chatHeader.jsx
@@ -0,0 +1,13 @@
+import React,{} from "react";
+import {
+  Box,
+  Typography
+} from "@mui/material"
+export default function chatHeader({receiver}) {
+  return(
+    <Box className="flex">
+     <img className="w-8 h-8 rounded-[50%]" src={receiver.image}/>
+     <Typography variant="h6">{receiver.name}</Typography>
+    </Box>
+    )
+}
