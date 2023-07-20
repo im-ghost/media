4756554edadfff645db179c34e2e06eef67ce7b8
Richard <culestfrosh@gmail.com>
2023-07-20 23:19:04 +0100

    fixed bugs

--- src/components/messages.jsx
+++ src/components/messages.jsx
@@ -1,13 +1,16 @@
-import {
-  Box
-} from "@mui/material";
-import React from "react";
-import Message from "./message"
-const Messages = ({ messages,user }) => {
+import { Box } from '@mui/material';
+import React from 'react';
+import Message from './message';
+const Messages = ({ messages, user }) => {
   return (
     <Box className="">
-    { messages.map((message)=> <Message message={message} isUsers{message.sender === user._id}/>) }
+      {messages.map((message) => (
+        <Message
+          message={message}
+          isUsers={message.sender === user._id}
+        />
+      ))}
     </Box>
-    )
-}
+  );
+};
 export default Messages;
