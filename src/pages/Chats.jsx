792010bcc25d58a7cdfc002b532f725ab7ce7e26
Richard <culestfrosh@gmail.com>
2023-07-20 23:17:43 +0100

    prettier

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,29 +1,27 @@
-import { Helemt } from "react-helmet";
-import {
-  Container
-} from "@mui/material";
-import ChatsList from "../components/chatsList";
-import Add from "../components/Add";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUser } from "../features/user/userslice";
-import React from "react";
+import { Helemt } from 'react-helmet';
+import { Container } from '@mui/material';
+import ChatsList from '../components/chatsList';
+import Add from '../components/Add';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/user/userslice';
+import React from 'react';
 const Chats = () => {
   const user = useSelector(selectUser);
-  if(user){
-  return(
-    <>
-    <Helemt>
-     <title> Chats Lists </title>
-    </Helemt>
-    <Container className="">
-     <ChatsList user={user}/>
-     <Add isCreatPost={false}/>
-    </Container>
-    </>
-    )
+  if (user) {
+    return (
+      <>
+        <Helemt>
+          <title> Chats Lists </title>
+        </Helemt>
+        <Container className="">
+          <ChatsList user={user} />
+          <Add isCreatPost={false} />
+        </Container>
+      </>
+    );
   }
-  return
-}
+  return;
+};
 
 export default Chats;
