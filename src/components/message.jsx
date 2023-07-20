5475701ab9033a90316153f7cb04f99aa8fbcd84
Richard <culestfrosh@gmail.com>
2023-07-20 23:18:49 +0100

    prettier

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,11 +1,15 @@
-import {} from "@mui/material";
-import React,{} from "react";
+import {} from '@mui/material';
+import React from 'react';
 
-export default function Message({message,isUsers}) {
+export default function Message({ message, isUsers }) {
   return (
-    <Box className={`p-2 rounded m-2   ${isUsers ? "justify-self-start" : "justify-self-end"}`}>
-     <Typography variant="body2">{message.timestamp}</Typography>
-     <Typography variant="body1">{message.message}</Typography>
+    <Box
+      className={`p-2 rounded m-2   ${
+        isUsers ? 'justify-self-start' : 'justify-self-end'
+      }`}
+    >
+      <Typography variant="body2">{message.timestamp}</Typography>
+      <Typography variant="body1">{message.message}</Typography>
     </Box>
-    )
+  );
 }
