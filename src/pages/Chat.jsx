a3afb36e20514c58d477da9e5f1bb3380113b425
Richard <culestfrosh@gmail.com>
2023-07-25 16:27:54 +0100

    updated chat page

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -10,17 +10,21 @@ import ChatHeader from '../components/chatHeader';
 import ChatFooter from '../components/chatFooter';
 import React, { useEffect, useState } from 'react';
 import { socket } from '../app/store';
-import { taost } from 'react-toastify';
+import { toast } from 'react-toastify';
 const Chat = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const user = useSelector(selectUser);
   const [messages, setMessages] = useState();
   const [chatId, setChatId] = useState();
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
   const [receiver, setReceiver] = useState();
-  const { data, error } = useGetChatByIdQuery(id);
+  const { data, error } = useGetChatByIdQuery({
+    id,
+    token:user.token,
+  });
   const send = async () => {
+    console.log(chatId);
     if (chatId) {
       socket.emit('sendMessage', {
         userId: user._id,
@@ -30,18 +34,21 @@ const Chat = () => {
     }
   };
   useEffect(() => {
-    if (true) {
+    if (chatId) {
       socket.on(`chatmessage-${chatId}`, (chat) => {
+        console.log(chat);
         setMessages(chat);
       });
     }
-  }, []);
+  }, [chatId]);
   useEffect(() => {
     if (data) {
-      setReceiver(data.receiver);
-      setMessages(data.messages);
-      setChatId(data.id);
-    } else {
+      setReceiver(data.chat.receiver);
+      setMessages(data.chat.messages);
+      setChatId(data.chat.id);
+      console.log(data);
+    } 
+    if(error){
       toast.error(JSON.stringify(error));
       navigate(-1);
     }
