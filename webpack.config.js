ea55665719a616a7f569f33d2dafd64ec0576985
Richard <culestfrosh@gmail.com>
2023-07-29 21:33:05 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.[contenthash].js',
+    filename: 'bundle.[contenthash].js',
     publicPath: '/',
   },
   module: {
