531364fd714e6a3d79022d9b8996fb2c969f6418
Richard <culestfrosh@gmail.com>
2023-07-29 09:13:40 +0100

    css: pages/Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 const Register = () => {
   return (
-    <div className="text-center shadow rounded-lg">
-      <Typography variant="h2">Register page</Typography>
+    <div className="text-center shadow rounded-lg bg">
+      <Typography variant="h2" className="header">Register page</Typography>
       <Reg1 />
     </div>
   );
