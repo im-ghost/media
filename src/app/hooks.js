c04a9c0b1898a01ae46b3d0f8d971d7d5cba2dbb
Richard <culestfrosh@gmail.com>
2023-07-29 17:29:06 +0100

    css: src/app

--- src/app/hooks.js
+++ src/app/hooks.js
@@ -1,5 +1,30 @@
 import { toast } from 'react-toastify';
 import axios from 'axios';
+import { formatDistanceToNow, format } from 'date-fns';
+
+export function formatDate(dDate) {
+  const date = new Date(dDate);
+  const currentDate = new Date();
+  const diffInMilliseconds = Math.abs(currentDate - date);
+
+  // Check if the difference is less than 1 minute
+  if (diffInMilliseconds < 60000) {
+    return 'just now';
+  }
+
+  // Check if the difference is less than 1 hour
+  if (diffInMilliseconds < 3600000) {
+    return formatDistanceToNow(date, { addSuffix: true });
+  }
+
+  // Check if the difference is less than 1 day
+  if (diffInMilliseconds < 86400000) {
+    return formatDistanceToNow(date, { addSuffix: true, includeSeconds: true });
+  }
+
+  // If it's beyond a day, return the date and time
+  return format(date, 'yyyy-MM-dd');
+}
 
 export const useNotify = async ({
   content, userId, token, authorId,
