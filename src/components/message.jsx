2f2f96d21c848bdd1df5eb8164da542e70c042f7
Richard <culestfrosh@gmail.com>
2023-07-29 09:13:38 +0100

    css: components/message.jsx

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -4,11 +4,11 @@ import {
 } from '@mui/material';
 import React from 'react';
 
-export default function Message({ message, isUsers }) {
+export default function Message({ message }) {
   return (
     <Box
-      className={`p-2 rounded m-2   ${
-        isUsers ? 'justify-self-start' : 'justify-self-end'
+      className={`p-2 rounded m-2 bg ${
+        message.direction === 'outgoing' ? 'justify-self-start' : 'justify-self-end'
       }`}
     >
       <Typography variant="body2">{message.timestamp}</Typography>
