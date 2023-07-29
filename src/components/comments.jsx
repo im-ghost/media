f863509d7f95c47a6a02224e87d066ce4ebd7802
Richard <culestfrosh@gmail.com>
2023-07-29 09:13:37 +0100

    css: components/comments.jsx

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from '@mui/material';
 const Comments = ({ comments, token, user }) => {
   return (
-    <Container className="p-2 h-auto">
+    <Container className="p-2 h-auto bg">
       {comments.map((comment) => (
         <Comment
           comment={comment}
