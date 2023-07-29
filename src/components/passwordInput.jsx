38be0fd6976cb12adfafac40811c1415e7c0b4ec
Richard <culestfrosh@gmail.com>
2023-07-29 09:13:38 +0100

    css: components/passwordInput.jsx

--- src/components/passwordInput.jsx
+++ src/components/passwordInput.jsx
@@ -10,7 +10,7 @@ export const Password = ({ value, onChange }) => {
     <TextField
       id="password"
       label="Password"
-      className="bg-gradient-to-r from-indigo-300 from-10% via-sky-300 via-40% to-emerald-300 to-100%  dark:from-blue-500 from-20% via-emarald-500 via-30% to-ryan-500 to-100% dark:text-amber-500 text-amber-800 rounded-lg"
+      className="bg rounded-lg"
       placeholder="Your password"
       type={passwordVisible ? 'text' : 'password'}
       InputProps={{
