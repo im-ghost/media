547cef4d7a1cf6e939c8d9e69e9a7dc3244c5cc7
Richard <culestfrosh@gmail.com>
2023-07-30 16:22:30 +0100

    ui: components/chatHeader.jsx

--- src/components/chatHeader.jsx
+++ src/components/chatHeader.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 export default function chatHeader({ receiver }) {
   return (
-    <Box className="flex bg">
+    <Box className="flex bg justify-evenly">
       <img
         className="w-8 h-8 rounded-[50%]"
         src={receiver.image}
       />
       <Typography variant="h6">{receiver.name}</Typography>
+      <div className="w-[40%]"></div>
     </Box>
   );
 }
