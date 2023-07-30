cd13648532250d3e3d3ff7dbf1b2a6c41581afc2
Richard <culestfrosh@gmail.com>
2023-07-30 16:22:31 +0100

    ui: components/message.jsx

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,10 +1,11 @@
 import { Box, Typography } from '@mui/material';
+import { formatDate } from '../app/hooks';
 import React from 'react';
 
 export default function Message({ message }) {
   return (
     <Box className="p-2 rounded m-2 bg justify-self-end h-auto min-h-12 min-w-12 max-w-24 w-20">
-      <Typography variant="body2">{message.timestamp}</Typography>
+      <Typography variant="body2">{formatDate(message.timestamp)}</Typography>
       <Typography variant="body1">{message.message}</Typography>
     </Box>
   );
