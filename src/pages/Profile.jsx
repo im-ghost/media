de15eb81de0bd0b69e8e9f0a78ff5c0667322459
Richard <culestfrosh@gmail.com>
2023-07-30 16:22:33 +0100

    ui: pages/Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,6 +12,7 @@ import {
   Tab,
   AppBar,
 } from '@mui/material';
+import Loader from '../components/loader';
 import { useGetUserByIdQuery } from '../features/user/userApiSlice';
 import { Helmet } from 'react-helmet';
 import React, { useEffect, useState, useLayoutEffect } from 'react';
@@ -73,11 +74,7 @@ const Profile = () => {
     }
   }, [data, error]);
   if (!user) {
-    return (
-      <Card onClick={() => navigate('/login')}>
-        <Typography variant="h1"> Login </Typography>
-      </Card>
-    );
+    return <Loader />;
   }
   return (
     <>
@@ -93,142 +90,140 @@ const Profile = () => {
           content={user.image ? user.image : require('../images/default.png')}
         />
       </Helmet>
-      <Container className="w-screen h-screen rounded flex items-center justify-center bg">
-        <Container className="w-[100vw] h-[100vh] bg">
-          <Card className="w-full bg">
-            <CardMedia
-              image={user.image ? user.image : require('../images/default.png')}
-              className="h-0 pt-[56.25%] mt-[30]"
-              sx={{
-                height: 0,
-                paddingTop: '56.25%',
-                marginTop: '30',
-              }}
-            />
-            <CardContent className="w-full text-center bg">
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                className="header"
-              >
-                {user.name}
-              </Typography>
-              <Typography
-                variant="body2"
-                color="text.secondary"
-              >
-                {user.bio}
-              </Typography>
-            </CardContent>
-            <CardActions className="w-full flex justify-between flex-row bg">
-              <Button
-                variant="contained"
-                size="small"
-              >
-                Edit Profile
-              </Button>
-              <Button
-                variant="contained"
-                size="small"
-              >
-                Delete Account
-              </Button>
-            </CardActions>
-          </Card>
-          <Paper
-            variant="elevation"
-            className="flex flex-wrap items-center justify-evenly h-[20vh] w-full bg"
-          >
-            <Box className="flex flex-col flex-wrap items-center justify-center bg">
-              <Typography variant="h6">Posts</Typography>
-
-              <Typography variant="body2">{user.posts.length}</Typography>
-            </Box>
-            <Box className="flex flex-col flex-wrap items-center justify-center bg">
-              <Link
-                to={`/follow`}
-                state={{
-                  data: {
-                    following: user.following,
-                    followers: user.followers,
-                  },
-                }}
-              >
-                <Typography variant="h6">Followers</Typography>
-
-                <Typography variant="body2">{user.followers.length}</Typography>
-              </Link>
-            </Box>
-            <Box className="flex flex-col flex-wrap items-center justify-center bg">
-              <Link
-                to={`/follow`}
-                state={{
-                  data: {
-                    following: user.following,
-                    followers: user.followers,
-                  },
-                }}
-              >
-                <Typography variant="h6">Following</Typography>
-                {user.following && (
-                  <Typography variant="body2">
-                    {user.following.length}
-                  </Typography>
-                )}
-              </Link>
-            </Box>
-          </Paper>
-          <Box className="w-full h-auto min-h-64 border bg">
-            <AppBar position="static">
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="secondary"
-                textColor="inherit"
-                variant="fullWidth"
-                aria-label="full width tabs example"
-              >
-                <Tab
-                  label="Posts"
-                  {...a11yProps(0)}
-                />
-                <Tab
-                  label="Retweets"
-                  {...a11yProps(1)}
-                />
-              </Tabs>
-            </AppBar>
-            <SwipeableViews
-              axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-              className="h-auto min-h-64 "
-              index={value}
-              onChangeIndex={handleChangeIndex}
+      <div className="w-[100vw] h-[100vh] bg">
+        <Card className="w-full bg">
+          <CardMedia
+            image={user.image ? user.image : require('../images/default.png')}
+            className="h-0 pt-[56.25%] mt-[30]"
+            sx={{
+              height: 0,
+              paddingTop: '56.25%',
+              marginTop: '30',
+            }}
+          />
+          <CardContent className="w-full text-center bg">
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="header"
             >
-              <TabPanel
-                value={value}
-                index={0}
-                dir={theme.direction}
-              >
-                <Posts
-                  posts={user.posts}
-                  token={user.token}
-                />
-              </TabPanel>
-              <TabPanel
-                value={value}
-                index={1}
-                dir={theme.direction}
-              >
-                <Posts
-                  posts={user.retweets}
-                  token={user.token}
-                />
-              </TabPanel>
-            </SwipeableViews>
+              {user.name}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+            >
+              {user.bio}
+            </Typography>
+          </CardContent>
+          <CardActions className="w-full flex justify-between flex-row bg">
+            <Button
+              variant="contained"
+              size="small"
+            >
+              Edit Profile
+            </Button>
+            <Button
+              variant="contained"
+              size="small"
+            >
+              Delete Account
+            </Button>
+          </CardActions>
+        </Card>
+        <Paper
+          variant="elevation"
+          className="flex flex-wrap items-center justify-evenly h-[20vh] w-full bg"
+        >
+          <Box className="flex flex-col flex-wrap items-center justify-center bg">
+            <Typography variant="h6">Posts</Typography>
+
+            <Typography variant="body2">{user.posts.length}</Typography>
           </Box>
-        </Container>
-      </Container>
+          <Box className="flex flex-col flex-wrap items-center justify-center bg">
+            <Link
+              to={`/follow`}
+              state={{
+                data: {
+                  following: user.following,
+                  followers: user.followers,
+                },
+              }}
+            >
+              <Typography variant="h6">Followers</Typography>
+
+              <Typography variant="body2">{user.followers.length}</Typography>
+            </Link>
+          </Box>
+          <Box className="flex flex-col flex-wrap items-center justify-center bg">
+            <Link
+              to={`/follow`}
+              state={{
+                data: {
+                  following: user.following,
+                  followers: user.followers,
+                },
+              }}
+            >
+              <Typography variant="h6">Following</Typography>
+              {user.following && (
+                <Typography variant="body2">{user.following.length}</Typography>
+              )}
+            </Link>
+          </Box>
+        </Paper>
+        <div className="w-screen h-auto min-h-64 border bg">
+          <AppBar position="static">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >
+              <Tab
+                label="Posts"
+                {...a11yProps(0)}
+              />
+              <Tab
+                label="Retweets"
+                {...a11yProps(1)}
+              />
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            className="h-auto min-h-64 w-screen"
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel
+              value={value}
+              index={0}
+              dir={theme.direction}
+              className="w-screen"
+            >
+              <Posts
+                posts={user.posts}
+                token={user.token}
+              />
+            </TabPanel>
+            <TabPanel
+              value={value}
+              index={1}
+              dir={theme.direction}
+              className="w-screen"
+            >
+              <Posts
+                posts={user.retweets}
+                token={user.token}
+              />
+            </TabPanel>
+          </SwipeableViews>
+        </div>
+      </div>
     </>
   );
 };
