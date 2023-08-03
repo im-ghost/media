0850d9fdf7337e2c6e0dabd25cfef7c6f6a03df4
Richard <culestfrosh@gmail.com>
2023-08-03 17:48:35 +0100

    service worker registrstion

--- src/index.jsx
+++ src/index.jsx
@@ -102,5 +102,14 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+     }).catch(registrationError => {
+       console.log('SW registration failed: ', registrationError);
+     });
+   });
+ }
 serviceWorkerRegistration.register();
 reportWebVitals();
