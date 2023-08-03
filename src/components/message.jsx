430fc871517722b92fc98e45c61266e1ad9dcf5b
Richard <culestfrosh@gmail.com>
2023-08-03 17:51:56 +0100

    styling

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,12 +1,16 @@
-import { Box, Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 import { formatDate } from '../app/hooks';
 import React from 'react';
 
-export default function Message({ message }) {
+export default function Message({ message, isUsers }) {
   return (
-    <Box className="p-2 rounded m-2 bg justify-self-end h-auto min-h-12 min-w-12 max-w-24 w-20">
+    <Paper
+      className={`p-2 rounded m-2 bg h-auto min-h-12 min-w-12 max-w-24 w-20 ${
+        isUsers ? 'justify-self-end' : 'justify-self-start'
+      }`}
+    >
       <Typography variant="body2">{formatDate(message.timestamp)}</Typography>
       <Typography variant="body1">{message.message}</Typography>
-    </Box>
+    </Paper>
   );
 }
