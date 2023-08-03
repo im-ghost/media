3984efc8197c755b61b498ac710dc0f576fc3e6d
Richard <culestfrosh@gmail.com>
2023-08-03 17:51:12 +0100

    isPage

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,7 +1,11 @@
 import { Typography } from '@mui/material';
 import React from 'react';
-const Loader = () => {
-  return <Typography variant="h1">Loading...</Typography>;
+const Loader = ({ isPage }) => {
+  return (
+    <div className={isPage ? 'page' : ''}>
+      <Typography variant="h1">Loading...</Typography>
+    </div>
+  );
 };
 
 export default Loader;
