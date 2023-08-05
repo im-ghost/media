41864d7a28f445593e5e98a2cf2970c760100f37
Richard <culestfrosh@gmail.com>
2023-08-05 08:33:22 +0100

    production

--- src/pages/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-const Logout = () => {
-  return (
-    <div>
-      <Typography variant="h1">Logout page</Typography>
-    </div>
-  );
-};
-export default Logout;
